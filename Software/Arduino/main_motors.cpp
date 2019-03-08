b93bcae0a15b2ddf1eea547380d89bfb990f7f1a
marrcogrova <marrcogrova@gmail.com>
2019-03-08 14:29:16 +0100

    added arduino code working............ i guess

--- Software/Arduino/main_motors.cpp
+++ Software/Arduino/main_motors.cpp
@@ -2,63 +2,76 @@
 #include <string.h>
 
 /*
-ENABLE |      PWM     |     DIG      |   STATUS           |
+ENABLE(PWM) |      A       |     B        |   STATUS           |
 --------------------------------------------------------- |
-LOW    |    ----      |    ----      |MOTOR PARADO        |
-HIGH   |    HIGH      |     LOW      |MOTOR GIRA DELANTE  |
-HIGH   |    LOW       |     HIGH     |MOTOR GIRA AL REVES |
-HIGH   |    HIGH      |     HIGH     |MOTOR PARADO        |
-HIGH   |    LOW       |     LOW      |MOTOR PARADO        |
+     LOW    |    ----      |    ----      |MOTOR PARADO        |
+     HIGH   |    HIGH      |     LOW      |MOTOR GIRA DELANTE  |
+     HIGH   |    LOW       |     HIGH     |MOTOR GIRA AL REVES |
+     HIGH   |    HIGH      |     HIGH     |MOTOR PARADO        |
+     HIGH   |    LOW       |     LOW      |MOTOR PARADO        |
  */
 
 /* Define the pin numbers */
-#define m1L_pwm 3
-#define m1L_dig 27
-#define m2L_pwm 5
-#define m2L_dig 29
-#define m3L_pwm 7
-#define m3L_dig 31
+#define m3L_a 31
+#define m3L_b 30
+#define m3L_en 2
 
-#define m1R_pwm 2
-#define m1R_dig 26
-#define m2R_pwm 4
-#define m2R_dig 28
-#define m3R_pwm 6
-#define m3R_dig 30 
+#define m2L_a 29
+#define m2L_b 28
+#define m2L_en 3
 
-#define enable_pH 14
+#define m1L_a 27
+#define m1L_b 26
+#define m1L_en 4
+
+#define m3R_a 25
+#define m3R_b 24
+#define m3R_en 5
+
+#define m2R_a 23
+#define m2R_b 22
+#define m2R_en 6
+
+#define m1R_a 21
+#define m1R_b 20
+#define m1R_en 7
 
 void setup() {
     Serial.begin(115200);
 
-    pinMode(m1L_pwm, OUTPUT); pinMode(m1L_dig, OUTPUT);
-    pinMode(m2L_pwm, OUTPUT); pinMode(m2L_dig, OUTPUT);
-    pinMode(m3L_pwm, OUTPUT); pinMode(m3L_dig, OUTPUT);
+    pinMode(m1L_a, OUTPUT); pinMode(m1L_b, OUTPUT);
+    pinMode(m2L_a, OUTPUT); pinMode(m2L_b, OUTPUT);
+    pinMode(m3L_a, OUTPUT); pinMode(m3L_b, OUTPUT);
 
-    pinMode(m1R_pwm, OUTPUT); pinMode(m1R_dig, OUTPUT);
-    pinMode(m2R_pwm, OUTPUT); pinMode(m2R_dig, OUTPUT);
-    pinMode(m3R_pwm, OUTPUT); pinMode(m3R_dig, OUTPUT);
+    pinMode(m1R_a, OUTPUT); pinMode(m1R_b, OUTPUT);
+    pinMode(m2R_a, OUTPUT); pinMode(m2R_b, OUTPUT);
+    pinMode(m3R_a, OUTPUT); pinMode(m3R_b, OUTPUT);
+
+    pinMode(m1L_en, OUTPUT); pinMode(m1R_en, OUTPUT);
+    pinMode(m2L_en, OUTPUT); pinMode(m2R_en, OUTPUT);
+    pinMode(m3L_en, OUTPUT); pinMode(m3R_en, OUTPUT);
 }
 
 
 /* function to control motor */
-void control_motor(int enable,int speed, int pwmPin, int DigPin){
+void control_motor(int enable,int speed, int dir1, int dir2){
     if(speed > 0){
         if (speed>125) speed=125;
-        digitalWrite(enable, HIGH);
-        analogWrite(pwmPin, speed);
-        digitalWrite(DigPin, LOW);
+        digitalWrite(dir1, HIGH);
+        digitalWrite(dir2, LOW);
+        analogWrite(enable, speed);
     }
     else if(speed < 0){
         speed=-speed;
         if (speed>125) speed=125;
-        digitalWrite(enable, HIGH);
-        analogWrite(pwmPin, speed);
-        digitalWrite(DigPin, HIGH);
+        digitalWrite(dir1, LOW);
+        digitalWrite(dir2, HIGH);
+        analogWrite(enable, speed);
     }
     else{
+        digitalWrite(dir1, LOW);
+        digitalWrite(dir2, LOW);
         digitalWrite(enable, LOW);
-        digitalWrite(DigPin, LOW);
     }
 }
 
@@ -86,13 +99,13 @@ void loop() {
     }
 
     // control motors
-    control_motor(enable_pH,speed[0], m1L_pwm,m1L_dig);
-    control_motor(enable_pH,speed[1], m2L_pwm,m2L_dig);
-    control_motor(enable_pH,speed[2], m3L_pwm,m3L_dig);
+    control_motor(m1L_en,speed[0], m1L_a,m1L_b);
+    control_motor(m2L_en,speed[1], m2L_a,m2L_b);
+    control_motor(m3L_en,speed[2], m3L_a,m3L_b);
 
-    control_motor(enable_pH,speed[3], m1R_pwm,m1R_dig);
-    control_motor(enable_pH,speed[4], m2R_pwm,m2R_dig);
-    control_motor(enable_pH,speed[5], m3R_pwm,m3R_dig);
+    control_motor(m1R_en,speed[3], m1R_a,m1R_b);
+    control_motor(m2R_en,speed[4], m2R_a,m2R_b);
+    control_motor(m3R_en,speed[5], m3R_a,m3R_b);
 
     // send messages to raspberry pi
     Serial.print(speed[0]); Serial.print(",");
