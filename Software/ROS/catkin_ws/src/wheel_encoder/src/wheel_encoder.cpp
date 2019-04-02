9b9e5871ca36e30d84b6cb4a711f05255184565b
marrcogrova <marrcogrova@gmail.com>
2019-04-02 17:00:33 +0200

    el caos se ha acercado a su fin

--- Software/ROS/catkin_ws/src/wheel_encoder/src/wheel_encoder.cpp
+++ Software/ROS/catkin_ws/src/wheel_encoder/src/wheel_encoder.cpp
@@ -6,30 +6,25 @@
 #include <std_msgs/Float32.h>
 #include <encoder.hpp>
 #include <pigpio.h>
-#include <cstdlib>
 
+/* Thread function */
+void read_encoderTh(int inc)
+{
+  static int pos = 0;
+  pos += inc;
+  std::cout << "pos=" << pos << std::endl;
+}
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "wheel_encoder");
   ros::NodeHandle n;
 
-  int *GPIO_vect;
-  re_decoder *encoders;
+  if (gpioInitialise() < 0) return 1;
 
-  GPIO_vect = (int*) malloc((argc-1)*sizeof(int));
-  encoders= (re_decoder*) malloc((argc-1)*sizeof(re_decoder));
+  re_decoder dec(7, read_encoderTh);
 
   ros::Publisher encoder_pub = n.advertise<std_msgs::Float32>("ticks_read", 1000);
-
-
-  /*Data type conversion and encoder init*/
-  for (int i=1;i<=argc;i++){
-    sscanf(argv[i],"%i",&GPIO_vect[i-1]);
-  }
-  for(int i=0;i;i++){
-    encoders[i]=re_decoder(GPIO_vect[i]) ;
-  }
 
   //sleep(3000);
   ros::Rate loop_rate(10); //10Hz
@@ -38,22 +33,19 @@ int main(int argc, char **argv)
   {
     std_msgs::Float32 data_vel;
 
-    data_vel.data=encoders[0].send_vel();
+    data_vel.data=dec.send_vel();
 
     encoder_pub.publish(data_vel);
 
-    ROS_INFO_STREAM("data sent in deg/sec: " << data_vel);
+    ROS_INFO_STREAM("data send in deg/sec: " << data_vel);
 
     ros::spinOnce();
     loop_rate.sleep();
   }
 
-  /* If something doesnt work, close. */
-  for (int i=1;i<=argc;i++){
-    encoders[i-1].re_cancel();
-  }
+  /* If something dont work, close. */
+  dec.re_cancel();
   gpioTerminate();
-  free(encoders);
-  free(GPIO_vect);
+
 return 0;
 }
