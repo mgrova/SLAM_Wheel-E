8888778da3f26aa134380e967ab73fc4047f0a9a
MikeMakes <mikemakes@protonmail.com>
2019-05-06 12:11:50 +0200

    pid paralelololo a ojojojojo

--- Software/ROS/catkin_ws/src/wheele/src/control.cpp
+++ Software/ROS/catkin_ws/src/wheele/src/control.cpp
@@ -15,6 +15,7 @@ geometry_msgs::Twist ref_ms;
 //std_msgs::float64 ref_ticks;
 wheele::pwm6 pwm_msg;
 float ticks;
+float kp=0.0,ki=0.0,kd=0.0;
 
 void get_ref(const geometry_msgs::Twist::ConstPtr& ref){
   ref_ms.linear.x=ref->linear.x;
@@ -25,6 +26,11 @@ void get_ticks(const std_msgs::Float64MultiArray::ConstPtr& ticks_read){
   ticks=ticks_read->data[0];
 }
 
+void get_pid(const std_msgs::Float64MultiArray::ConstPtr& pid){
+  kp=pid->data[0];
+  ki=pid->data[1];
+  kd=pid->data[2];
+}
 
 float ms2ticks(float ms){
   float tickss;
@@ -34,14 +40,25 @@ float ms2ticks(float ms){
 }
 
 int main(int argc, char **argv){
+  //std::ofstream myfile ("pwm.log",std::ios::app |std::ios::out);
+  std::chrono::time_point<std::chrono::system_clock> t_init,t_act,t_lastT;
+  t_init = std::chrono::system_clock::now();
+  t_act=t_lastT=t_init;
+  std::chrono::duration<double> dt;
+
   ros::init(argc, argv, "control");
   ros::NodeHandle n;
   ros::Subscriber ref_sub = n.subscribe("cmd_vel",100,get_ref);
   ros::Subscriber ticks_sub = n.subscribe("encoders_ticks",100,get_ticks);
   ros::Publisher pwm_pub = n.advertise<wheele::pwm6>("pwm", 100);
+
+  ros::Subscriber pid_sub = n.subscribe("pid",10,get_pid);
+
   ros::Rate loop_rate(10); //10Hz
 
-static float sat_err=0,uk1=0;
+  static float sat_err=0,uk1=0,i=0;
+  static float ukns,ukreal,sat_err_integrated,ek,ek1;
+  float p,d,ueq=0;
 
 while(n.ok()) {
   float lin, ang, left_ref_ms, right_ref_ms, left_ref_ticks, right_ref_ticks;
@@ -52,22 +69,22 @@ while(n.ok()) {
   left_ref_ticks = ms2ticks(left_ref_ms);
   right_ref_ticks = ms2ticks(right_ref_ms);
 
-  static float uk1,ukns,ukreal,sat_err_integrated,ek,ek1;
-static float ukns1;
-  float p1=1, z1=1.3433, nd1=121.5684;
-  sat_err_integrated=sat_err*0.1;
+  t_act = std::chrono::system_clock::now();
+  dt=t_act-t_lastT;
+
+  sat_err_integrated=sat_err*dt.count();
   ek=left_ref_ticks-ticks;
-  ukns=p1*uk1+(z1+nd1)*ek1;
-  ukns=ukns+(sat_err_integrated/0.1)+ueq;
-//ukns=ukns+ueq;
+
+  p = kp*ek;
+  i = ki*(i+ek*dt.count());
+  d = kd*((ek-ek1)/dt.count());
+  ukns=p+i+d+(sat_err_integrated/0.1)+ueq;
   ROS_INFO_STREAM("ukns:  "<<ukns <<"\n");
   if(ukns<-255) ukreal=-255;
   else if(ukns>255) ukreal=255;
   else ukreal=ukns;
   sat_err=ukreal-ukns;
-//ukns1=ukns;
-  uk1=ukreal;
-ek1=ek;
+  ek1=ek;
 
   ROS_INFO_STREAM("ukreal:  "<<ukreal <<"\n");
 
@@ -81,6 +98,7 @@ ek1=ek;
 
   ros::spinOnce();
   loop_rate.sleep();
+  t_lastT=t_act;
   }
   return 0;
 }
