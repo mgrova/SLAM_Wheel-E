d21f3c506dfc4592c103db8083987fa24c7831f6
MikeMakes <mikemakes@protonmail.com>
2019-05-12 18:03:49 +0200

    little a little

--- Software/ROS/catkin_ws/src/wheele/src/control.cpp
+++ Software/ROS/catkin_ws/src/wheele/src/control.cpp
@@ -14,17 +14,18 @@
 #define radius 0.035  // Radius of your wheels
 #define enc_slits 22  // nº of slits in your encoders
 #define pi 3.141592   // un arco de circunferencia entre su radio o algo asi
+#define d_wheels 0.250    // distance between wheels
 
-float kp=0.0,ki=0.0,kd=0.0; // pid gains  
+float kp=0.0,ki=0.0,kd=0.0; // pid gains
 
 float p,d,sat_err_integrated,ek,ukns,ukreal,ueq=0;
 //static float ek1_m1l, sat_err=0,i=0;
-static float ek1_m1l,sat_err_m1l=0,i_m1l=0;
-static float ek1_m1r,sat_err_m1r=0,i_m1r=0;
-static float ek1_m2l,sat_err_m2l=0,i_m2l=0;
-static float ek1_m2r,sat_err_m2r=0,i_m2r=0;
-static float ek1_m3l,sat_err_m3l=0,i_m3l=0;
-static float ek1_m3r,sat_err_m3r=0,i_m3r=0;
+static float ek1_m1l=0,sat_err_m1l=0,i_m1l=0;
+static float ek1_m1r=0,sat_err_m1r=0,i_m1r=0;
+static float ek1_m2l=0,sat_err_m2l=0,i_m2l=0;
+static float ek1_m2r=0,sat_err_m2r=0,i_m2r=0;
+static float ek1_m3l=0,sat_err_m3l=0,i_m3l=0;
+static float ek1_m3r=0,sat_err_m3r=0,i_m3r=0;
 
 static float kp_m1L,kp_m2L,kp_m3L,kp_m1R,kp_m2R,kp_m3R;
 static float ki_m1L,ki_m2L,ki_m3L,ki_m1R,ki_m2R,ki_m3R;
@@ -51,6 +52,7 @@ void ticksCb(const std_msgs::Float64MultiArray::ConstPtr& ticks_read){
   ticks.m1r=ticks_read->data[3];
   ticks.m2r=ticks_read->data[4];
   ticks.m3r=ticks_read->data[5];
+
 }
 
 void dynCb(wheele::controlParamsConfig &config, uint32_t level)
@@ -73,18 +75,18 @@ void dynCb(wheele::controlParamsConfig &config, uint32_t level)
   kp_m3R=config.Kp_m3R;
   ki_m3R=config.Ki_m3R;
 
-  std::cout << "Params control m1L->  Kp:" << kp_m1L <<" Ki:" << ki_m1L <<"\n";
-  std::cout << "Params control m2L->  Kp:" << kp_m2L <<" Ki:" << ki_m2L <<"\n";
-  std::cout << "Params control m3L->  Kp:" << kp_m3L <<" Ki:" << ki_m3L <<"\n";
-  std::cout << "Params control m1R->  Kp:" << kp_m1R <<" Ki:" << ki_m1R <<"\n";
-  std::cout << "Params control m2R->  Kp:" << kp_m2R <<" Ki:" << ki_m2R <<"\n";
-  std::cout << "Params control m3R->  Kp:" << kp_m3R <<" Ki:" << ki_m3R <<"\n"; 
+  //std::cout << "Params control m1L->  Kp:" << kp_m1L <<" Ki:" << ki_m1L <<"\n";
+  //std::cout << "Params control m2L->  Kp:" << kp_m2L <<" Ki:" << ki_m2L <<"\n";
+  //std::cout << "Params control m3L->  Kp:" << kp_m3L <<" Ki:" << ki_m3L <<"\n";
+  //std::cout << "Params control m1R->  Kp:" << kp_m1R <<" Ki:" << ki_m1R <<"\n";
+  //std::cout << "Params control m2R->  Kp:" << kp_m2R <<" Ki:" << ki_m2R <<"\n";
+  //std::cout << "Params control m3R->  Kp:" << kp_m3R <<" Ki:" << ki_m3R <<"\n";
 }
 
 /* FUNCTIONS */
 float ms2ticks(float ms){
   float tickss;
-  tickss=ms/(2*pi*radius); // [m/s] to [rad/s] (wheel radius = 0.035)
+  tickss=ms/radius; // [m/s] to [rad/s] (wheel radius = 0.035)
   tickss=tickss*(enc_slits/(2*pi)); // [rad/s] to [ticks/s] (encoders have 22 slits)
   return tickss;
 }
@@ -96,38 +98,36 @@ float claw(int m, std::chrono::duration<double> dt, float ref, float out, float 
   switch(m) {
     case 1: kp=kp_m1L;
             ki=ki_m1L;
-            kd=.01;
+            kd=.0;
             break;
     case 2: kp=kp_m1R;
             ki=ki_m1R;
-            kd=.01;
+            kd=.0;
             break;
     case 3: kp=kp_m2L;
             ki=ki_m2L;
-            kd=0.01;
+            kd=0.0;
             break;
     case 4: kp=kp_m2R;
             ki=ki_m2R;
-            kd=.01;
-            break;                                    
+            kd=.0;
+            break;
     case 5: kp=kp_m3L;
             ki=ki_m3L;
-            kd=.01;
+            kd=.0;
             break;
     case 6: kp=kp_m3R;
             ki=ki_m3R;
-            kd=.01;
+            kd=.0;
             break;
-   
   }
-  std::cout << "kp: " << kp << "ki: " << ki << "kd: " << kd << std::endl;
+  //std::cout << "kp: " << kp << "ki: " << ki << "kd: " << kd << std::endl;
   p = kp*ek;
   i = ki*(i+ek*dt.count());
   d = kd*((ek-ek1)/dt.count());
-  std::cout << "p: " << p << "i: " << i << "d: " << d << std::endl;
+  //std::cout << "p: " << p << "i: " << i << "d: " << d << std::endl;
   ukns=p+i+d+(sat_err_integrated/0.01)+ueq;  // PID + antiwindup + offset'
 
-  //ROS_INFO_STREAM("ukns:  "<<ukns <<"\n");
   if(ukns<-255) ukreal=-255;  // Saturation
   else if(ukns>255) ukreal=255;
   else ukreal=ukns;
@@ -137,6 +137,15 @@ float claw(int m, std::chrono::duration<double> dt, float ref, float out, float 
     case 1: sat_err_m1l=sat_err;
             i_m1l=i;
             ek1_m1l=ek1;
+
+            std::cout << "ref " << ref << "\n";
+            std::cout << "out " << out << "\n";
+            std::cout << "ek " << ek << "\n";
+            std::cout << "p " << p << "\n";
+            std::cout << "i " << i << "\n";
+            std::cout << "ukns " << ukns << "\n";
+            std::cout << "\n";
+
             break;
     case 2: sat_err_m1r=sat_err;
             i_m1r=i;
@@ -182,14 +191,14 @@ int main(int argc, char **argv){
   f = boost::bind(&dynCb, _1, _2);
   srv.setCallback(f);
 
-  ros::Rate loop_rate(10); //10Hz
+  ros::Rate loop_rate(100); //10Hz
 
 while(n.ok()) {
   float lin, ang, left_ref_ms, right_ref_ms, left_ref_ticks, right_ref_ticks;
   lin=ref_ms.linear.x;
   ang=ref_ms.angular.z;
-  left_ref_ms =lin - (ang*radius)/2.0;
-  right_ref_ms =lin + (ang*radius)/2.0;
+  left_ref_ms =lin - (ang*d_wheels)/2.0;
+  right_ref_ms =lin + (ang*d_wheels)/2.0;
   left_ref_ticks = ms2ticks(left_ref_ms);
   right_ref_ticks = ms2ticks(right_ref_ms);
 
@@ -212,7 +221,7 @@ while(n.ok()) {
   //ROS_INFO_STREAM("pwm m3l:  "<<pwm_msg.m3l <<"\n");
   //pwm_msg.m3r=claw(6 ,dt, right_ref_ticks, ticks.m3r, ek1_m3r, sat_err_m3r, i_m3r);
   //ROS_INFO_STREAM("pwm m3r:  "<<pwm_msg.m3r <<"\n");
-  error_m1L.data=ek1_m2l;
+  error_m1L.data=ek1_m1l;
   pub_err.publish(error_m1L);
   pwm_pub.publish(pwm_msg);
 
